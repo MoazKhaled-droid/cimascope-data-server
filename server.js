f5d3dd100a99579eb11ec06943b429417b31af0e
MoazKhaled-droid <moazkhaled.dev@gmail.com>
2026-09-16 03:20:27 +0300

    fix: cors sittengs

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const jsonServer = require('json-server');
 const cors = require('cors');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+
+const router = jsonServer.router('db.json'); 
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
+const corsOptions = {
+  origin: [
+    'https://cimascope.vercel.app', 
+    'http://localhost:5173'        
+  ],
+  optionsSuccessStatus: 200
+};
+
+server.use(cors(corsOptions));
 
 server.use(middlewares);
 server.use(router);
